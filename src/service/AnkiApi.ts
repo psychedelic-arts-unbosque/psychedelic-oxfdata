da7ba25df5faad983c93128c1330c9854081d536
afmunozm <afmunozm@unbosque.edu.co>
2019-12-08 18:13:38 -0500

    last changes to anki api

--- src/service/AnkiApi.ts
+++ src/service/AnkiApi.ts
@@ -27,12 +27,8 @@ export default class AnkiApi {
                 }
             });
     
-            xhr.open('POST', "http://ec2-107-23-129-31.compute-1.amazonaws.com:8080/");
+            xhr.open('POST', "http://ec2-107-23-129-31.compute-1.amazonaws.com:3000/addWord");
             xhr.setRequestHeader("Content-Type", "application/json");
-            xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-            xhr.setRequestHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-            xhr.setRequestHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
-            xhr.withCredentials = true;
             xhr.send(JSON.stringify({action, version, params}));
         });
     }
